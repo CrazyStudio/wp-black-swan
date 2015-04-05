c5f18db6aa083788b15fa4109c7282646ae484ff
Mateusz Rybin <thecrazypolishguy@gmail.com>
2015-04-05 16:19:56 +0200

    Make the menu icon animate and insert the SVG file into the HTML
    I made the icon animate with SMIL but also needed to insert the SVG data
    into the HTML file.
    
    I tried to use the "document.getElementById" to traget the ID of the svg
    elements but it didn't work, because the SVG is in a separate file. This
    makes it a problem. I don't have a solution execpt doing some weried
    code form a 3-party that I don't understand yet.
    
    WHY
    I want it to work right now and then worry about it later.
    
    Signed-off-by: Mateusz Rybinski <rybinmateusz@gmail.com>

--- js/app.js
+++ js/app.js
@@ -2,13 +2,31 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$("#site-navigation-toggle").on("click", function(event) {
+var icon_grid = document.getElementById("main-button");
+var animationToGreen = document.getElementById("animation-to-green");
+var animationToYellow = document.getElementById("animation-to-yellow");
 
-	console.log(event);
+$("#site-navigation-toggle").on( "click", function() {
 
 	$("#site-navigation").toggle();
 
-	
-
+	if (icon_grid.classList.contains("toggle-animation")) {
+    icon_grid.classList.remove("toggle-animation");
+    animationToYellow.beginElement();
+  	} else {
+    icon_grid.classList.add("toggle-animation");
+    animationToGreen.beginElement();
+	}
 
 });
+
+
+
+if (document.getElementById('top-3') != null) {
+    var str = document.getElementById("top-3").value;
+    alert("Hello! I am an alert box!!");
+}
+else {
+    var str = null;
+    alert("Hello! I am an alert box!!!!!!!!!!!!!!!");
+}
