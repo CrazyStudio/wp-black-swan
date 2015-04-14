f05ab320c81e13cda884a3688a9560bb6c7a70f6
Mateusz Rybin <thecrazypolishguy@gmail.com>
2015-04-14 17:36:20 +0200

    Make the toggle text change when you click
    I use the data-attr to make it possible to change the language. I
    didn't hard code the name: Explore and close to make sure that
    it will be possible to change lanuguage later.
    
    Signed-of-by: Mateusz Rybinski <rybinmateusz@gmail.com>

--- js/app.js
+++ js/app.js
@@ -5,17 +5,28 @@ $(document).foundation();
 var icon_grid = document.getElementById("main-button");
 var animationToGreen = document.getElementById("animation-to-green");
 var animationToYellow = document.getElementById("animation-to-yellow");
+var text = $('#toggle-title');
 
 $("#site-navigation-toggle").on( "click", function() {
 
+	// Open and close the navigation
 	if($("#site-navigation").hasClass("toggle")) {
 		$("#site-navigation").removeClass("toggle");
 	} else {
 		$("#site-navigation").addClass("toggle");
 	}
 
-	$("#site-header").animate({ 'min-height': "59px" }, '400', 'linear');
+	//Change the text of the toggle
+	if($("#toggle-title").text() === "Explore" ) {
 
+		$("#toggle-title").text($("#toggle-title").data("close"));
+	} else {
+		$("#toggle-title").text($("#toggle-title").data("open"));
+	}
+
+
+
+	// Animate the menu button
 	if (icon_grid.classList.contains("js-css-toggle-animation")) {
     icon_grid.classList.remove("js-css-toggle-animation");
     animationToYellow.beginElement();
