fa10b1091c648a5fe04915dc1e17ca1072feca20
Mateusz Rybin <thecrazypolishguy@gmail.com>
2015-04-15 17:46:32 +0200

    Refactor whole project except index.html#'site-content'

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$( document ).foundation();
 
-var icon_grid = document.getElementById("main-button");
-var animationToGreen = document.getElementById("animation-to-green");
-var animationToYellow = document.getElementById("animation-to-yellow");
-var text = $('#toggle-title');
+var icon_grid = document.getElementById( "main-button" );
+var animation_to_close = document.getElementById( "animation-to-close" );
+var animation_to_explore = document.getElementById( "animation-to-explore" );
 
-$("#site-navigation-toggle").on( "click", function() {
+
+// Make the navigation toggle button work
+$( "#site-navigation-toggle" ).on( "click", function() {
 
 	// Open and close the navigation
-	if($("#site-navigation").hasClass("toggle")) {
-		$("#site-navigation").removeClass("toggle");
+	if ($( "#site-navigation" ).hasClass( "toggle" )) {
+		$( "#site-navigation" ).removeClass( "toggle" );
 	} else {
-		$("#site-navigation").addClass("toggle");
+		$( "#site-navigation" ).addClass( "toggle" );
 	}
 
-	//Change the text of the toggle
-	if($("#toggle-title").text() === "Explore" ) {
-
-		$("#toggle-title").text($("#toggle-title").data("close"));
+	//Change the text of the toggle title
+	if ($( "#toggle-title" ).text() === "Explore" ) {
+		$( "#toggle-title" ).text($( "#toggle-title" ).data( "close" ));
 	} else {
-		$("#toggle-title").text($("#toggle-title").data("open"));
+		$( "#toggle-title" ).text($( "#toggle-title" ).data( "open" ));
 	}
 
 	// Animate the menu button
-	if (icon_grid.classList.contains("js-css-toggle-animation")) {
-    icon_grid.classList.remove("js-css-toggle-animation");
-    animationToYellow.beginElement();
+	if ( icon_grid.classList.contains( "js-css-toggle-animation" ) ) {
+    icon_grid.classList.remove( "js-css-toggle-animation" );
+    animation_to_explore.beginElement();
   	} else {
-    icon_grid.classList.add("js-css-toggle-animation");
-    animationToGreen.beginElement();
+    icon_grid.classList.add( "js-css-toggle-animation" );
+    animation_to_close.beginElement();
 	}
 
 	// Fix a bugg in scroll function when the menu is to high (on weed...)
-	if($(".header-fixed").css("bottom") === "auto") {
-		$(".header-fixed").css("bottom", "0px")
+	if ($( ".header-fixed" ).css( "bottom" ) === "auto" ) {
+		$( ".header-fixed" ).css( "bottom", "0px" );
 	} else {
-		$(".header-fixed").css("bottom", "auto")
+		$( ".header-fixed" ).css( "bottom", "auto" );
 	}
 });	
