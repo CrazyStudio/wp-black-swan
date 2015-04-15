058edaf330e19b1af34f5ff5fe8b037fde11e09f
Mateusz Rybin <thecrazypolishguy@gmail.com>
2015-04-15 20:06:32 +0200

    Init function file and create header.php

---
+++ header.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="site-content">
+ *
+ * @package Black Swan
+ */
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+
+<!-- meta data -->
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<!-- stylesheet and script -->
+<script src="/js/svg4everybody.ie8.min.js"></script>
+<script src="bower_components/modernizr/modernizr.js"></script>
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="inc/font-awesome.min.css">
+<!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"> -->
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<!-- SVG System Ref: https://css-tricks.com/svg-sprites-use-better-icon-fonts/ -->
+<?php get_template_part( svg, defs ); ?> 
+	
+<div id="page" class="hfeed site header-fixed">
+	
+		<header id="site-header" class="site-header" role="banner">
+
+		<div class="top-header row">
+
+			<div id="site-branding" class="site-branding small-12 medium-9 small-centered medium-uncentered columns">
+	        	<h1 class="site-title">
+	        		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+	        			<img src="<?php echo bloginfo('template_directory') . '/images/logo.svg'; ?>" alt="<?php bloginfo( 'name' ); ?>">
+	        		</a>
+	        	</h1>
+	            <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	        </div> <!-- site-branding -->
+
+	        <div id="site-navigation-toggle" class="site-navigation-toggle small-8 medium-3 small-centered medium-uncentered columns">
+                    <button>
+                        <span id="toggle-title" data-open="Explore" data-close="Close">Explore</span>
+                        <svg id="main-button" class="icon-grid">
+                            <use xlink:href="#icon-grid">
+                            </use>
+                        </svg>
+                    </button> 
+                </div> <!-- site-navigation-toggle -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ svg-defs.php
@@ -0,0 +1,28 @@
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" enable-background="new 0 0 100 100" xml:space="preserve" style="display: none;">
+  <defs>
+    <symbol id="icon-grid" viewBox="0 0 30 30">
+      <title>Basketball</title>
+        <g>
+            <path d="M22,22 L25.1758929,22 L28,22 L28,28 L22,28 L22,22 Z" id="dow-3"></path>
+            <path d="M0,22 L3.87388393,22 L6,22 L6,28 L0,28 L0,22 Z" id="dow-1"></path>
+            <path d="M22,0 L24.712995,0 L28,0 L28,6 L22,6 L22,0 Z" id="top-3"></path>
+            <path d="M0,0 L3.28835404,0 L6,0 L6,6 L0,6 L0,0 Z" id="top-1"></path>
+            <animate id="animation-to-close" begin="indefinite" fill="freeze" attributeName="opacity" dur="300ms" to="0"></animate>
+            <animate id="animation-to-explore" begin="indefinite" fill="freeze" attributeName="opacity" dur="300ms" to="1"></animate>
+        </g>
+        <path d="M11,22 L13.1125388,22 L17,22 L17,28 L11,28 L11,22 Z" id="dow-2"></path>
+        <path fill="000" d="M22,11 L24.5763587,11 L28,11 L28,17 L22,17 L22,11 Z" id="mid-3"></path>
+        <path d="M11,11 L14.716974,11 L17,11 L17,17 L11,17 L11,11 Z" id="mid-2"></path>
+        <path d="M0,11 L3.20243595,11 L6,11 L6,17 L0,17 L0,11 Z" id="mid-1"></path>
+        <path d="M11,0 L14.017139,0 L17,0 L17,6 L11,6 L11,0 Z" id="top-2"></path>
+    </symbol>
+
+    <symbol id="icon-search" viewBox="0 0 92 92">
+        <title>search</title>
+        <path d="M64.02,58.467c5.146-6.379,7.942-14.266,7.942-22.556c0-19.884-16.172-36.061-36.05-36.061
+        c-19.879,0-36.051,16.177-36.051,36.061c0,19.876,16.172,36.052,36.051,36.052c8.415,0,16.385-2.869,22.816-8.149l25.793,26.037
+        l5.34-5.288L64.02,58.467z M35.865,62.862c-14.863,0-26.951-12.09-26.951-26.953c0-14.867,12.088-26.958,26.951-26.958
+        c14.856,0,26.951,12.09,26.951,26.958C62.816,50.772,50.722,62.862,35.865,62.862z"/>
+    </symbol>
+  </defs>
+</svg>
