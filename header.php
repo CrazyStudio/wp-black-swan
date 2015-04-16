44f999d6fe574e987280812c87baa16d4e3f7efc
Mateusz Rybin <thecrazypolishguy@gmail.com>
2015-04-16 18:11:14 +0200

    refactor and create content.php

--- header.php
+++ header.php
@@ -21,11 +21,11 @@
 
 <body <?php body_class(); ?>>
 <!-- SVG System Ref: https://css-tricks.com/svg-sprites-use-better-icon-fonts/ -->
-<?php get_template_part( svg, defs ); ?> 
+<?php get_template_part( 'inc/parts/svg', 'system' ); ?> 
 	
 <div id="page" class="hfeed site header-fixed">
 	
-		<header id="site-header" class="site-header" role="banner">
+	<header id="site-header" class="site-header" role="banner">
 
 		<div class="top-header row">
 
@@ -53,51 +53,36 @@
 		<nav id="site-navigation" class="site-navigation" role="navigation">
 
 			<div id="site-primary-menu" class="site-primary-menu">
-
-			<?php
-
-				$defaults = array(
+			<?php $defaults = array(
 					'container' 		=> false,
 					'theme_location' 	=> 'primary',
 					'menu_class' 		=> 'row',
 					'menu_id'			=> 'primary-menu'
 					);
-
-				wp_nav_menu( $defaults );
-
-			?>
-
+				wp_nav_menu( $defaults ); ?>
 			</div> <!-- site-primary-menu -->
 			
 			<div id="site-search" class="site-search row">
-
                 <?php get_search_form(); ?>
-
             </div> <!-- site-search -->
 
-            <div id="site-social" class="site-social small-centered small-12 columns">
-                    
+            <div id="site-social" class="site-social small-centered small-12 columns"> 
                 <a href="http://facebook.com/"><i class="fa fa-facebook"></i></a>
                 <a href="http://twitter.com/"><i class="fa fa-twitter"></i></a>
                 <a href="http://instagram.com/"><i class="fa fa-instagram"></i></a>
                 <a href="http://pinterest.com/"><i class="fa fa-pinterest"></i></a>
                 <a href="http://plus.google.com/"><i class="fa fa-google-plus"></i></a>
                 <a href="http://"><i class="fa fa-tumblr"></i></a>
-                <a href="http://youtube.com/"><i class="fa fa-youtube-play"></i></a>
-                    
+                <a href="http://youtube.com/"><i class="fa fa-youtube-play"></i></a>      
             </div> <!-- site-social -->
-			
 
+        </nav> <!-- site-navigation -->
 
+    </header> <!-- site-header -->
 
+</div> <!-- header-fixed -->
 
-
-
-
-				</div>
-				</header>
-				</div>
-
+<div id="site-content" class="site-content">
 
 
 
--- index.php
+++ index.php
@@ -13,4 +13,12 @@
 
 get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						
+		<?php get_template_part('inc/layout/content', '' ); ?>
+			
+	<?php endwhile; ?>
+	
+	<?php endif; ?>
+
 <?php get_footer(); ?>
