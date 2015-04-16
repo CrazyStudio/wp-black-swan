cbbd89986cd9c59c42edae23c8bab4181529f3a6
Mateusz Rybin <thecrazypolishguy@gmail.com>
2015-04-16 15:43:09 +0200

    finish the header

--- footer.php
+++ footer.php
@@ -2,11 +2,12 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after
+ * Contains the closing of the #site-content div and all content after
  *
  * @package Black Swan
  */
 ?>
+</div> <!-- site-content: Found bottom of header.php -->
 
 <?php wp_footer(); ?>
 
